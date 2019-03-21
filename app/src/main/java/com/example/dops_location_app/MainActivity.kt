41d65a486cf439cab597d6a2244c0666b9bcf0f8
Sergio Aguilar <svega@republicservices.com>
2019-03-21 15:14:59 -0700

    Change git email

--- app/src/main/java/com/example/dops_location_app/MainActivity.kt
+++ app/src/main/java/com/example/dops_location_app/MainActivity.kt
@@ -23,6 +23,7 @@ open class MainActivity : AppCompatActivity() {
     companion object {
         const val MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
         const val fineLocationPermission = Manifest.permission.ACCESS_FINE_LOCATION
+
     }
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
